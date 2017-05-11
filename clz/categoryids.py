2f6f5483ff2197a1b373e9189494ad6d4d6f1484
Joseph Rawson <joseph.rawson.works@gmail.com>
2017-05-11 16:45:46 -0500

    added superhero functions

--- clz/categoryids.py
+++ clz/categoryids.py
@@ -6,6 +6,7 @@ import requests
 
 CATEGORYIDS_URL = 'http://pics.ebay.com/aw/pics/pdf/us/file_exchange/CategoryIDs-US.csv'
 CategoryIDFields = ['CategoryID', 'Category Path']
+CAT_PREFIX = 'Collectibles > Comics'
 
 def download_url(url):
     local_filename = url.split('/')[-1]
@@ -51,3 +52,47 @@ def make_csv(filename, prefix):
         for cid, cpath in catlist:
             writer.writerow(dict(zip(fieldnames, [cid, cpath])))
     
+
+
+
+
+# superhero stuff
+def get_superhero_categories(config):
+    catlist = get_categories(CAT_PREFIX)
+    cdata = {}
+    table = []
+    ages = config.options('comic_ages')
+    # no individual platinum/golden age superheroes
+    oldages = ['platinum', 'golden']
+    ages = [a for a in ages if a not in oldages]
+    superheroes = dict()
+    for cid, cpath in catlist:
+        clist = [x.strip() for x in cpath.split(' > ')]
+        clist = clist[2:]
+        cdata[cid] = clist
+        age = clist[0].split()[0].lower()
+        if age in ages:
+            #print "AGE", age, clist
+            hero = clist[1]
+            if hero == 'Superhero':
+                #print "%s age superhero %s" % (age, clist[2])
+                table.append((cid, age, clist[2]))
+        elif age in oldages:
+            if age == 'platinum':
+                table.append((cid, age, clist[0]))
+            elif age == 'golden' and clist[1] == 'Superhero':
+                table.append((cid, age, clist[1]))
+    return table
+
+def make_superhero_table(config):
+    table = get_superhero_categories(config)
+    rows = []
+    for cid, age, hero in table:
+        years = config.get('comic_ages', age)
+        start, end = [int(x.strip()) for x in years.split(',')]
+        rows.append((cid, age, start, end, hero))
+    return rows
+    
+        
+        
+            
